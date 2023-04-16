f335c60994e91e838f87eb6fb31971b04940a0a1
Narmin <mnermin064@gmail.com>
2023-04-16 11:01:07 +0400

    Javascript enter, data types

---
+++ Lesson11/assets/js/index.js
@@ -0,0 +1,59 @@
+// alert('Girisiniz ugurlu deyil')
+// prompt('Yasinizi daxil edin')
+// console.log('Javascript External');
+
+// Data types
+// Primitive data type - Besit data tipi 
+// String data type - Setir tipi
+
+var a = 'Vusal';
+console.log(a);
+
+// Number data type - Reqem tipi
+
+var b = 12;
+console.log(b);
+
+// Boolean data type 
+
+var d = true;
+var e = false;
+
+console.log(d);
+console.log(e);
+
+
+// Non-primitive - Murekkeb data tipi
+
+// Object data type - Obyekt data tipi(Coxluq)
+
+var user = {
+    name :'Vusal',
+    age : 15,
+    city : "Baku"
+}
+
+console.log(user);
+console.log('Your city is ' + user.city);
+console.log('Your age is ' + user.age);
+
+
+
+// Array data type - Massiv data tipi
+
+var myObject = ['ilkin', 123, true, null, undefined]
+console.log(myObject);
+console.log(myObject[3]);
+
+// Trivial data type - Xususi data tipi
+
+
+// Null data type - 
+
+var q = null;
+console.log(q);
+
+// Undefined data type - 
+
+var z ;
+console.log(z);
