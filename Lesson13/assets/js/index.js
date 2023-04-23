32e12d92a1be1037f6fe3b7f1a78ac592333eb84
Narmin <mnermin064@gmail.com>
2023-04-23 19:01:18 +0400

    Practice lesson

---
+++ Lesson13/assets/js/index.js
@@ -0,0 +1,59 @@
+// Arithmetic Operators 
+// + 
+// - 
+// * 
+// /  
+// %  
+// **  
+// ++  
+// --  
+
+
+//  Assingment Operators
+// = 
+// +=  
+// -=  
+// *=  
+// /=  
+// %=  
+// **= 
+
+
+
+// Comparison operators 
+// ==   
+// ===  
+// !=   
+// !==  
+// <     
+// <=    
+// >     
+// >= 
+
+
+// Logical operator
+
+// &&  - və 
+// ||  - və ya
+// !   - fərqlidir
+
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,            (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+// \            (tərs slash)(Qaçındırma operatorudur)
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+// ?:           (sual üst-üstə qoşa nöqtə)((şərt) ? ifadə1 : ifadə2) Ternary operator !!!
+// şərt            ? true                 : false
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+// delete       (objectin dəyərini silmək üçündür)
+// new          (yeni dəyər yaratməq üçündür)
+// this         (mövcud dəyəri işarələmək üçündür)
+// in           (dəyişkənin(object) daxilində dəyərin olub olmamasını yoxlamaq üçündür)
+// instanceof   (dəyişkənin(objectin xüsusiyyətinin) içində xüsusiyyət olub olmamasını yoxlamaq üçündür)
+
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
+
+
